805ec782d76f688e16a88d9f3e811584ad2425db
anujaghegadmal <80381704+anujaghegadmal@users.noreply.github.com>
2021-03-10 18:02:42 +0530

    Add files via upload

---
+++ file_1.py
@@ -0,0 +1,23 @@
+students=[
+    {
+        "name":"Anuja",
+        "roll_no":"102"
+    },
+    {
+        "name":"Tanuja",
+        "roll_no":"103"
+    },
+    {
+        "name":"Rutuja",
+        "roll_no":"104"
+    },
+    {
+        "name":"vijya",
+        "roll_no":"105"
+    }
+]
+
+for i in students:
+    if i["roll_no"]=="103":
+        print(i)
+        
